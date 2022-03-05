5afc3953b6c34ed5f45d9034c90a9666aefc6d67
Fatih <ahmetfatihcenesizdev@gmail.com>
2022-03-05 11:10:31 +0300

    files uploaded

---
+++ src/main/kotlin/ceesiz/demo/springboot/demoWithKotlin01/DemoWithKotlin01Application.kt
@@ -0,0 +1,11 @@
+package ceesiz.demo.springboot.demoWithKotlin01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoWithKotlin01Application
+
+fun main(args: Array<String>) {
+	runApplication<DemoWithKotlin01Application>(*args)
+}
---
+++ src/test/kotlin/ceesiz/demo/springboot/demoWithKotlin01/DemoWithKotling01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ceesiz.demo.springboot.demoWithKotlin01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoWithKotling01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
